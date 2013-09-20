953c1f5daf318bf8a3eb80ed4ea95cdbf249d917
brendan <brendan.heberton@gmail.com>
2013-09-20 17:38:35 -0600

    working on flow for mobile

--- app/scripts/collections/closest-collection.js
+++ app/scripts/collections/closest-collection.js
@@ -8,23 +8,36 @@ function( App, Backbone ) {
 	/* Return a collection class definition */
 	return Backbone.Collection.extend({
 		initialize: function() {
-			console.log("initialize a Closest collection");
-      var self = this;
-
+			App.vent.on('update:focus:mountain', this.getClosest, this);
 		},
 
-    getClosest: function() {
+    getClosest: function( mountain ) {
       
       var self = this;
       
-      //TODO replace res with actual keyed mountain from user location
-      var res = App.mountains_all.models[ 90 ];
-      var closest = res.get( 'feature' ).properties.closest;
-      
-      _.each( closest, function( name, i ) {
-        var m = App.mountains_all.findWhere( { name: name.replace(/\s+/g, '') } );
+      this.reset();
+
+      var res, closest;
+      if ( !mountain ) {
+        res = App.mountains_all.models[ 80 ];
+        res.set('index', 4);
+        closest = res.get( 'feature' ).properties.closest;
+      } else {
+        res = mountain;
+        res.set('index', 4);
+        closest = mountain.get( 'feature' ).properties.closest;
+      }
+
+      App.activeMountain = res; //for navigation, keep track of which mountain is 'active'
+
+      for (var i = 0; i<8; i++ ) {
+
+        if ( i === 4 ) self.add( res ); // stuff the selected mountain in the middle of array
+        var m = App.mountains_all.findWhere( { name: closest[ i ].replace(/\s+/g, '') } );
+        m.set('index', null);
         self.add( m );
-      });
+
+      };
 
     }
 		
