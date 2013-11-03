f24c0b21abefb6643cc87f530343609031178f7e
brendan <brendan.heberton@gmail.com>
2013-11-02 23:32:44 -0600

    stying points, stupid stupid text

--- app/scripts/lib/d3Layer.js
+++ app/scripts/lib/d3Layer.js
@@ -14,15 +14,25 @@ if(typeof(d3Layer)=='undefined'){
 
       //calculate bounds for outer extent of points
       var bounds = d3.geo.bounds(collection),
-          path = d3.geo.path().projection(project).pointRadius(function (d) { return 6 });
+          path = d3.geo.path().projection(project).pointRadius(function (d) { 
+            var radus = getRadius( d.properties.current_new );
+            if ( d.properties.current_status === "closed" ) radius = 4;
+            return radius;
+          });
         
       //each point 
       //TODO add open/closed logic
       var feature = g.selectAll("path")
           .data(collection.features)
         .enter().append("path")
-          .attr('class', "mtn");
-     
+          .attr('class', function(d) {
+            if ( d.properties.current_status === "closed" ) {
+              return "mtn mtn-closed";
+            } else {
+              return "mtn";
+            }
+          });
+
       //bind to reset event for zooming
       App.map.on("viewreset", reset);
       
@@ -50,11 +60,68 @@ if(typeof(d3Layer)=='undefined'){
         //update path ( points )
         feature.attr("d", path);
       }
-     
+
+      /*
+      GET WORKING!
+      svg.selectAll(".mtn-label")
+        .data(collection.features)
+      .enter().append("text")
+        .attr("class", "mtn-label")
+        .attr("transform", function(d) { return "translate(" + project([d.geometry.coordinates[0],d.geometry.coordinates[1]]) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) { return "10" });
+      */
+
       // Use Leaflet to implement a D3 geographic projection.
       function project(x) {
         var point = App.map.latLngToLayerPoint(new L.LatLng(x[1], x[0]));
         return [point.x, point.y];
+      }
+
+      function addText() {
+        
+      }
+
+      /*
+      * Calculate radius for new snow markers
+      */
+      function getRadius(snow) {
+        if ( !snow ) snow = 0;
+
+        switch (true) {
+        case (snow == 0):  
+          radius = 6;
+          text = 10;
+          break;
+        case (snow == 1):  
+          radius = 10;
+          break;
+        case (snow <= 3):
+          radius = 12;
+          break;
+        case (snow <= 5):
+          radius = 14;
+          text = 13;
+          break;
+        case (snow <= 10):
+          radius = 16;
+          text = 14;
+          break;
+        case (snow > 10 && snow < 15):
+          radius = 19;
+          text = 15;
+          break;
+        case (snow >= 15 && snow < 20):
+          radius = 23;
+          text = 16;
+          break;
+        case (snow >= 20):
+          radius = 28;
+          text = 18;
+          break;
+        default:
+          radius = 7;
+      }
       }
     }
   }
